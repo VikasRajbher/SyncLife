c66c9e9935d7f5015d8f0b1005f1b295b46d6359
VikasRajbher <vikasrajbher@gmail.com>
2026-02-24 13:13:23 +0530

    api connection in frontend

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -27,7 +27,7 @@ export default function Register() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        `${import.meta.env.VITE_API_URL}/api/auth/register`,
         formData,
       );
 
